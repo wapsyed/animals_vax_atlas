35de4587e5d7e4885ef02de4860ca54a9fefd801
Wasim Syed <wasimvacinas@gmail.com>
2025-11-25 14:01:18 -0300

    Add MSigDB Hallmarks analysis for E. coli infection
    Introduced MSigDB Hallmarks gene set enrichment analysis for E. coli infection in human and mouse, including new code in Frameworks.Rmd and 2_Comparing_Human_Mouse.Rmd. Added generation and saving of hallmark process gene and mean process tables as RDS files. Also made minor improvements to notebook structure and enabled required package installation in 2_Comparing_Human_Mouse.Rmd.

--- scripts_notebooks/required.R
+++ scripts_notebooks/required.R
@@ -32,16 +32,16 @@ lapply(bioc_pkgs, library, character.only = TRUE)
 #   install.packages(cran_missing)
 # }
 # 
-# # # Install packages from Bioconductor
-# # # Install BiocManager if necessary
-# # if (!requireNamespace("BiocManager", quietly = TRUE)) {
-# #   install.packages("BiocManager")
-# # }
-# # 
-# # bioc_missing <- setdiff(bioc_pkgs, rownames(installed.packages()))
-# # if (length(bioc_missing) > 0) {
-# #   BiocManager::install(bioc_missing)
-# # }
+# # Install packages from Bioconductor
+# # Install BiocManager if necessary
+# if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#   install.packages("BiocManager")
+# }
+# 
+# bioc_missing <- setdiff(bioc_pkgs, rownames(installed.packages()))
+# if (length(bioc_missing) > 0) {
+#   BiocManager::install(bioc_missing)
+# }
 # 
 # # Load packages (CRAN + Bioconductor)
 # invisible(lapply(c(required_packages, bioc_pkgs), function(pkg) {
