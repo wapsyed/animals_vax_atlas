393978c38c140742c07031d237d14d1b8baa23b7
Wasim Syed <wasimvacinas@gmail.com>
2026-07-13 17:36:03 -0300

    Update - Correlation

--- scripts_notebooks/required.R
+++ scripts_notebooks/required.R
@@ -13,7 +13,7 @@ cran_pkgs <- c(
   "readxl", "gt", "pracma", "ggnewscale", "ggprism", "ggtext", "devtools", 
   "ggpp", "corto", "Hmisc", "patchwork", "tidymodels", "TidyDensity", "forcats", 
   "GGally", "ggbeeswarm", "geomtextpath", "ggfx", "rstatix", "matrixTests",
-  "pkgconfig"
+  "pkgconfig", "wCorr"
 )
 
 bioc_pkgs <- c(
